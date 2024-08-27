78a82b1064f3d2f237084fb039b129efab125f7e
NicolasKonishi <145286206+NicolasKonishi@users.noreply.github.com>
2024-08-27 12:40:40 -0300

    Criando controller para contatos

---
+++ WebAPI.Curso-DIO/Controllers/ContatoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPI.Curso_DIO.Context;
+using WebAPI.Curso_DIO.Entities;
+
+namespace WebAPI.Curso_DIO.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContatoController:ControllerBase
+    {
+        private readonly AgendaContext _context;
+
+        public ContatoController(AgendaContext context) { 
+            
+            _context = context;
+        
+        }
+        [HttpPost]
+        public IActionResult Create(Contato contato)
+        {
+            _context.Add(contato);
+            _context.SaveChanges();
+            return Ok(contato);
+        }
+    }
+}
