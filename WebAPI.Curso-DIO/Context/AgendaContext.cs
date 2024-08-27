e140b26965e123708b0f34aa6d58389ffefebf1b
NicolasKonishi <145286206+NicolasKonishi@users.noreply.github.com>
2024-08-27 12:31:16 -0300

    criando as migrations com Entity Framework

--- WebAPI.Curso-DIO/Context/AgendaContext.cs
+++ WebAPI.Curso-DIO/Context/AgendaContext.cs
@@ -1,6 +1,14 @@
-﻿namespace WebAPI.Curso_DIO.Context
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Curso_DIO.Entities;
+
+namespace WebAPI.Curso_DIO.Context
 {
     public class AgendaContext : DbContext
     {
+        public AgendaContext(DbContextOptions<AgendaContext> options) : base(options) {
+
+
+        }
+        public DbSet<Contato> Contatos {get; set;}
     }
 }
